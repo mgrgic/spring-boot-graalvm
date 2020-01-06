a51cad55ee917b4ab0726f06ed627cb5edb38412
Miki Grgic <m@grgic.de>
2020-01-06 18:00:05 +0100

    usage of maven graal-native plugin for build

--- graalvm-hellospring/src/main/java/de/graalvm/HelloSpring.java
+++ graalvm-hellospring/src/main/java/de/graalvm/HelloSpring.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class HelloSpring implements CommandLineRunner {
 
     public static void main(String[] args) {
