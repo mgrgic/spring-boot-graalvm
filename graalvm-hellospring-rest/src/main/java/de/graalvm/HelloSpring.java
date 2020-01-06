680c264f44df1bea0f3989e14d1e1cae40ea2740
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-01-06 18:25:10 +0100

    Apply version changes

--- graalvm-hellospring-rest/src/main/java/de/graalvm/HelloSpring.java
+++ graalvm-hellospring-rest/src/main/java/de/graalvm/HelloSpring.java
@@ -3,7 +3,7 @@ package de.graalvm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class HelloSpring {
 
     public static void main(String[] args) {
