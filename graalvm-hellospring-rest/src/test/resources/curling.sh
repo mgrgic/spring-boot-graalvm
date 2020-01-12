2665b99c088e6dc643773d39e3ac678a4587f439
Miki Grgic <m@grgic.de>
2020-01-12 17:11:20 +0100

    added Dockerfile for Spring Boot Jar App; added REST curl test script

---
+++ graalvm-hellospring-rest/src/test/resources/curling.sh
@@ -0,0 +1,14 @@
+curl http://localhost:8080/hello
+printf "\n---------------------\n"
+curl -H "Content-Type: application/json" http://localhost:8080/lombok
+printf "\n---------------------\n"
+curl -H "Content-Type: application/json" http://localhost:8080/pojo
+printf "\n---------------------\n"
+curl -H "Content-Type: application/json" http://localhost:8080/serialized
+printf "\n---------------------\n"
+curl -H "Content-Type: application/json" http://localhost:8080/builder
+printf "\n---------------------\n"
+curl -H "Content-Type: application/json" http://localhost:8080/simplemap
+printf "\n---------------------\n"
+curl -H "Content-Type: application/json" http://localhost:8080/map
+printf "\n---------------------\n"
